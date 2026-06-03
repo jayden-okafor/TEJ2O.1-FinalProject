9c5ef663f347b195fda1867be35be4cf5d31a7fc
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-06-03 13:27:27 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -5,4 +5,17 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber8 = robotbit.Servos.S1
+
+// show happy face
+basic.showIcon(IconNames.Happy)
+
+// when button "A" is clicked
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString('0')
+    basic.showIcon(IconNames.SmallSquare)
+    robotbit.Servo(servoNumber8, 170)
+    basic.showIcon(IconNames.Happy)
+})

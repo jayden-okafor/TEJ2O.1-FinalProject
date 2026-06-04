4e13f71f87551ec26fc06d61647822febd3bd040
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-06-03 23:26:35 -0400

    made a test code to test the servo heading

--- main.ts
+++ main.ts
@@ -1,21 +1,45 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jayden and Josiah
+ * Created on: Jun 2026
+ * This program automatically opens a trash can when something gets close to it. and closes it back when the object moves away
 */
+
+// show happy face
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 
 // variables
 const servoNumber8 = robotbit.Servos.S1
 
-// show happy face
-basic.showIcon(IconNames.Happy)
+/*
+while (true) {
 
-// when button "A" is clicked
+    // get the distance using the sonar
+    const distance = sonar.ping(
+        DigitalPin.P1, // trigger
+        DigitalPin.P2, // echo
+        PingUnit.Centimeters,
+    )
+
+    if (distance > 0 && distance <= 10) {
+        robotbit.Servo(servoNumber8, 118)
+        basic.pause(3500)
+    } else {
+        robotbit.Servo(servoNumber8, 180)
+    }
+}
+*/
+
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    basic.showString('0')
-    basic.showIcon(IconNames.SmallSquare)
-    robotbit.Servo(servoNumber8, 170)
+    robotbit.Servo(servoNumber8, 118)
     basic.showIcon(IconNames.Happy)
 })
+
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber8, 180)
+    basic.showIcon(IconNames.Happy)
+})
+
+// dead center == 118 degreees
+// left == 180 degrees
